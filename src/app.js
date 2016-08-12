1e3cdad34fb5b1e11a55856891f02381bbab551c
Daniel Lim <hbt@markhamboards.com>
2016-08-12 11:24:47 -0400

    that's it for now, going to take a break now!
    that's it for now, going to take a break now!

--- src/app.js
+++ src/app.js
@@ -3,13 +3,18 @@ class Sports {
       this._id = id;
   }
 
-  get id() {
+  get id() { // id is not a function either. it is a property.
     return this._id;
+  }
 
+  set id(value) {
+    this._id = value;
   }
 }
 
 let sport = new Sports('S001');
+
+sport.id = 's005'; // new value!
 
 console.log("1: " + sport);
 console.log("1a: " + sport.id);
@@ -18,3 +23,5 @@ console.log("1b: " + sport._id);
 console.log("2: " + Sports);
 console.log("3: " + Sports.id);
 console.log("4: " + Sports._id);
+
+// that's it for now, going to take a break now!
