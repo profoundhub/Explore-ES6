885a18571559f58d592539854b9db93c88a3f2cb
Daniel Lim <hbt@markhamboards.com>
2016-08-12 10:54:11 -0400

    getting there
    getting there

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 class Sports {
   constructor(sportName, id) {
-      console.log('Name of Sport: '
-      + sportName, id);
-
+      // console.log('Name of Sport: ' + sportName, id);
+      this.sportName = sportName;
+      this.id = id;
   }
 }
 let sport = new Sports('Swimming', 'S001');
+
+console.log('Sport: ' + sport.sportName + ' ' + sport.id);
 
 // console.log(sportName);
 // console.log(sport instanceof Sports);
