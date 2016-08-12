e1150aea22020f270604b7be7c8941ba3fc8c868
Daniel Lim <hbt@markhamboards.com>
2016-08-12 14:19:13 -0400

    Clean up
    Clean up

--- src/app.js
+++ src/app.js
@@ -8,17 +8,3 @@ let dataService = new SportsDataServices();
 dataService.loadData(sportsData);
 
 console.log(dataService.mens);
-
-// console.log(sportsData);
-
-/*
-let w = new Womens();
-let m = new Mens();
-
-console.log(w);
-console.log(m);
-*/
-
-/* All ready for Part 3 ... yes this setup was just that -- setup/ setting up everything */
-// The Fun Begins now ...!
-// with Data Services.
