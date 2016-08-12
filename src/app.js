825539d3ebecdf76e252a6b61652e256c6b19cdb
Daniel Lim <hbt@markhamboards.com>
2016-08-12 11:21:44 -0400

    get id
    get id

--- src/app.js
+++ src/app.js
@@ -1,50 +1,20 @@
 class Sports {
-  constructor(id, name) {
-      // console.log('Name of Sport: ' + sportName, id);
-      this.id = id;
-      this.name = name;
+  constructor(id) {
+      this._id = id;
   }
 
-  // Static Methods:
+  get id() {
+    return this._id;
 
-  static getGender() {
-    console.log('men or women?');
-  }
-
-
-  // Methods (looks like a function, but is it?)
-  location() {
-    console.log('I am ' + this.name + ' in Rio');
   }
 }
-Sports.olymics = 2016; // Static Properties.
 
-let sport = new Sports('S001','Swimming');
-let sport2 = new Sports('S002','Fencing');
+let sport = new Sports('S001');
 
-Sports.getGender();
+console.log("1: " + sport);
+console.log("1a: " + sport.id);
+console.log("1b: " + sport._id);
 
-sport.location();
-sport2.location();
-
-// console.log();
-
-// console.log('ID #1: ' + sport.id + ' & Sport #2 ID: ' + sport2.id);
-// console.log('Year ' + Sports.olymics); // Static Properties.
-
-// console.log('Sport #1 ID: ' + sport.id + ' & Sport #2 ID: ' + sport2.id);
-// Let's simplify that:  console.log('Sport: ' + sport.sportName + ' ' + sport.id);
-// console.log('Sport ID: ' + sport.id] + ' & Sport Name: ' + sport.name);
-// console.log('Sport ID: ' + sport['id'] + ' & Sport Name: ' + sport['name']);
-
-// console.log(sportName);
-// console.log(sport instanceof Sports);
-
-
-/* What we have learnt so far ...
-
-Sports = function (... class === function ? )
-sport = object
-sport instanceof Sports = true (boolean)
-
-*/
+console.log("2: " + Sports);
+console.log("3: " + Sports.id);
+console.log("4: " + Sports._id);
